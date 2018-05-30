1986f91a4736e59fd45a39b229e70c234c7f0e64
abeprincec <abeprincec@gmail.com>
2018-05-30 00:29:44 +0000

    made foreign key migration

---
+++ migrations/20180530001922_add_fk_famous_people.js
@@ -0,0 +1,19 @@
+
+exports.up = function(knex, Promise) {
+    return Promise.all([
+        knex.schema.table('milestones', function (table) {
+            
+            table.foreign('id').references('famous_people.id')
+          })
+        ])
+};
+
+
+exports.down = function(knex, Promise) {
+    return Promise.all([
+      knex.schema.table('milestones', function(table) {
+        table.dropColumn('id')
+      })
+    ])
+  };
+  
